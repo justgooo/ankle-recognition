de9385a0b9cb66683b1d5a825ecc3f46ca0e5001
lox <loxgt.xy@gmail.com>
2026-04-05 08:38:15 +0800

    R6-03 rerun: set asymmetric fusion temperature to 0.3

--- src/model.py
+++ src/model.py
@@ -262,7 +262,7 @@ class MultiViewDecisionFusionClassifier(MultiViewEncoder):
         - temperature → ∞：逐渐退化为平均化融合
     """
 
-    DEFAULT_TEMPERATURE = 1.0
+    DEFAULT_TEMPERATURE = 0.3
 
     def __init__(
         self,
