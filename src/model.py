4c5236373f39b0a29c6c12df1bbe4663a03f97af
lox <loxgt.xy@gmail.com>
2026-04-05 09:52:40 +0800

    R6-05 rerun: restore temperature 0.5 and lower lr

--- src/model.py
+++ src/model.py
@@ -262,7 +262,7 @@ class MultiViewDecisionFusionClassifier(MultiViewEncoder):
         - temperature → ∞：逐渐退化为平均化融合
     """
 
-    DEFAULT_TEMPERATURE = 2.0
+    DEFAULT_TEMPERATURE = 0.5
 
     def __init__(
         self,
